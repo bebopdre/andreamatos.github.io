e4fd5bacc975723bc49a69c1155d3be4a1d29ca9
andreaairplane <andreadrematos@gmail.com>
2024-04-03 21:35:41 -0400

    added photo and little rocket

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -6,6 +6,16 @@ function HomePage() {
             <div className='stars'></div>
             <div className='stars2'></div>
             <div className='stars3'></div>
+
+            <section className='home__picture'>
+                <div className='home__picture-img'></div>
+                <div className='ship'>
+                    <div className='ship-rotate'>
+                        <div className='pod'></div>
+                        <div className='fuselage'></div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
